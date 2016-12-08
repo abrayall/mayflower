5c818df890e3a1b3aaba14a62b003650d2ddc656
Aaron Brayall <abrayall@yahoo.com>
2016-12-08 18:49:59 -0500

    incrementing version to v1.1.0

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -x VERSION=1.0.1
+declare -x VERSION=1.1.0
 declare -x HEADER="// mayflower.js v$VERSION"
 
 rm -rf build
@@ -9,8 +9,11 @@ mkdir -p build/work
 echo $HEADER > build/mayflower.js
 echo $HEADER > build/mayflower.min.js
 
+echo "[build] building mayflower v$VERSION..."
+echo "[build] generating mayflower.js..."
 cat src/main/javascript/lang.js src/main/javascript/widget.js >> build/mayflower.js
 cp build/mayflower.js build/mayflower-$VERSION.js
 
+echo "[build] generating mayflower.min.js..."
 curl -X POST -s --data-urlencode "input@build/mayflower.js" https://javascript-minifier.com/raw >> build/mayflower.min.js
 cp build/mayflower.min.js build/mayflower-$VERSION.min.js
